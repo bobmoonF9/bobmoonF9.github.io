535000516d047685860ba1e9ee399ec3b28fa83a
bobmoonf9 <111763634+bobmoonF9@users.noreply.github.com>
2022-11-03 10:15:50 -0400

    script

--- Content/js/test.js
+++ Content/js/test.js
@@ -1,17 +1,16 @@
 
-alert (window.location.href);
+alert ('current page URL is ' + window.location.href);
 
 // supported languages
 var langs = [ 'en-us', 'fr-ca', 'de' ];
 var path = location.pathname;
 var curLang = "en-us";
 var lang;
-var rootURL;	  		//store the current URL up to the language 
 var subfolders;			// the portions of the URL between the language and the topic file name
 var contentID;			// the portion of the URL relating to the content repo
 var landingpageURL;  	// construct the url of the landing page 
 
-$(document).ready(function() {
+$(document).ready(function() {			// on page load, find which language folder is in the URL
 	for (var i = 0; i < langs.length; i++) {
 		if (path.indexOf('/' + langs[i] + '/') != -1) {
 			curLang = langs[i];
@@ -20,8 +19,7 @@ $(document).ready(function() {
 });
 
 
-$(document).ready(function() { 
-	// set language selector current language
+$(document).ready(function() { 			// on page load, set the language drop down to the current language
 	$('select#lang').val(curLang);
 	$('select#lang').css('visibility','visible');
 
@@ -35,46 +33,29 @@ $(document).ready(function() {
 
 
 // redirect
-function use_lang() {
+function use_lang() {					// on change to the language drop down
 	for (var i = 0; i < langs.length; i++) {
 		if (path.indexOf('/' + langs[i] + '/') != -1) {
 			curLang = langs[i];
 		}
 	}
 
-	var filename = path.substring(path.lastIndexOf('/') + 1);
+	var filename = path.substring(path.lastIndexOf('/') + 1); 	// sets current topic file name, not used in the current script
 	if (path.indexOf('/' + lang + '/') == -1) {
-		var url = window.location.toString();
+		var url = window.location.toString();					// set the url variable to current path	
 		
-		rootURL = url.slice(url.StartsWith, url.search(curLang) ); 	// find the URL up to the current language
+		rootURL = url.slice(url.StartsWith, url.search(curLang) ); 	// grab the URL up to the current language
 
-		subfolders = url.substring(rootURL.length + curLang.length + 1,url.lastIndexOf("/"))  ; //get everything starting at the root URL to the last slash
+		subfolders = url.substring(rootURL.length + curLang.length + 1,url.lastIndexOf("/")); //grab the folders in the URL from the rootURL location to the last slash
 		
-		//check if the subfolders var has a slash
+		//check if the subfolders var has a slash, so the page is nested in subfolders under the guide level
 		if (subfolders.indexOf('/') == -1) {	
-			contentID = subfolders; 	// if no slash, set the variable 
-			//alert ( 'content3 if no slash  = ' + content3);
+			contentID = subfolders; 	// if no slash for subfolders, set the variable 
 		} else {
-			//alert ( 'content2 in if = ' + content2);		
-			contentID = subfolders.substring(0,subfolders.indexOf("/")) ;     // else chop the info after the first slash 
-			//alert ( 'content3 after adjustment if no slash  = ' + content3);
+			contentID = subfolders.substring(0,subfolders.indexOf("/")) ;     // else chop the subfolders after the first slash 
 		}
 
-		landingpageURL = rootURL + lang + "/" + contentID + "/landing-" + contentID + ".htm" ;   // combine elements to make the landing page URL
-		
-		
-		//landingpage = url.slice(url.StartsWith, url.search(curLang)); 			
-			//alert ( 'URL string = ' + landingpage  );
-		//content1 = url.lastIndexOf(curLang);
-		//content1 = url.slice(url.StartsWith, url.search(url.lastIndexOf(curLang)));
-			//alert (content1);
-		//landingpage= url.slice(url.StartsWith, url.search(curLang)) + "/Content/" + "landing-" +landingpage + ".htm"; 	
-			//alert ( 'URL string = ' + landingpage  );
-		//landingpage= url.slice(url.StartsWith, url.search(curLang)) + "/Content/" + "landing-" +landingpage + ".htm"; 	
-			//alert ( 'URL string = ' + landingpage  );
-		//url = url.replace(curLang, lang);
-		//url = url.substring(0, url.indexOf(lang)) + lang + "/Content/" + "landing-" +landingpage + ".htm";
-		url = landingpageURL;
+		url = rootURL + lang + "/" + contentID + "/landing-" + contentID + ".htm" ;   // combine elements to make the landing page URL
 		window.location = url;
 	}
 }
