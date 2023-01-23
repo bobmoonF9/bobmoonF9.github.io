f2fbecea89b7fb268eb6302b583157ab46ecf21c
bobmoonf9 <111763634+bobmoonF9@users.noreply.github.com>
2023-01-23 11:14:34 -0500

    Update test.js

--- Content/js/test.js
+++ Content/js/test.js
@@ -56,8 +56,8 @@ function use_lang() {					// on change to the language drop down
 		
 		//overrides for testing
 		// var url = "https://documentation.five9.com/de/crm-agents/ADT/morefolders/test/_ch-preparing-your-station.htm";
-		var url = https://documentation.five9.com/de/Content/crm-agents/ADT/softphone/preparing-your-station.htm
-		// var url = https://documentation.five9.com/de/Content/crm-agents/NetSuite/engagement/_ch-processing-digital-engagement-interactions.htm
+		var url = https://documentation.five9.com/de/Content/crm-agents/ADT/softphone/preparing-your-station.htm;
+		// var url = https://documentation.five9.com/de/Content/crm-agents/NetSuite/engagement/_ch-processing-digital-engagement-interactions.htm;
 		
 		curLang = "de";											// for testing, always use de
 		lang = "de";
