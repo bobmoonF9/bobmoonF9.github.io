93dc66449364b1c70535911f7b6a59d1960ba1ee
bobmoonf9 <111763634+bobmoonF9@users.noreply.github.com>
2023-01-23 10:52:18 -0500

    changes

--- Content/js/test - Copy (2).js
+++ Content/js/test - Copy (2).js
@@ -18,12 +18,11 @@ var subfolders;			// the portions of the URL between the language and the topic 
 var contentID;			// the portion of the URL relating to the content repo
 var integrationID;		// the crm integration indicator from the URL
 var integrationPath; 	// the path for crm integrations (contentID/integrationID)
-var landingPage; 		// the landing page topic for projects that don't follow the naming convention
 
 $(document).ready(function() {			// on page load, find which language folder is in the URL
 	for (var i = 0; i < langs.length; i++) {
 		if (path.indexOf('/' + langs[i] + '/') != -1) {
-			curLang = langs[i];			// set variable based on URL
+			curLang = langs[i];
 		}
 	}
 });
@@ -50,6 +49,11 @@ function use_lang() {					// on change to the language drop down
 		}
 	}
 
+
+
+
+
+
 	var filename = path.substring(path.lastIndexOf('/') + 1); 	// sets current topic file name, not used in the current script
 	if (path.indexOf('/' + lang + '/') == -1) {
 		var url = window.location.toString();					// set the url variable to current path	
@@ -57,40 +61,43 @@ function use_lang() {					// on change to the language drop down
 		//overrides for testing
 		var url = "https://documentation.five9.com/de/crm-agents/ADT/morefolders/_ch-preparing-your-station.htm";
 		// https://documentation.five9.com/de/Content/crm-agents/ADT/softphone/preparing-your-station.htm
-		curLang = "de";											// for testing, always use de
+		curLang = "de";
 		lang = "de";
 		
 		rootURL = url.slice(url.StartsWith, url.search(curLang) ); 	// grab the URL up to the current language
+
 		subfolders = url.substring(rootURL.length + curLang.length + 1,url.lastIndexOf("/")); //grab the folders in the URL from the rootURL location to the last slash
 		
 		//check if the subfolders var has a slash, so the page is nested in subfolders under the guide level
 		if (subfolders.indexOf('/') == -1) {	
-			contentID = subfolders; 										// if no slash for subfolders, set the variable 
+			contentID = subfolders; 	// if no slash for subfolders, set the variable 
 		} else {
-			contentID = subfolders.substring(0,subfolders.indexOf("/")) ;   // else chop the subfolders after the first slash 
+			contentID = subfolders.substring(0,subfolders.indexOf("/")) ;     // else chop the subfolders after the first slash 
 		}
 
 
 		if(contentID=="crm-agents") {	// process the crm-agents and crm-admin content which has a different structure
-		  integrationID = subfolders.substring(contentID.length + 1); 		// set integrationID to the first level of the subfolders
-		  integrationID = integrationID.substring(0,integrationID.indexOf("/")); // chop off extra subfolders
-		  //alert ("includes a crm subfolder = " + integrationID );  
+		  integrationID = subfolders.substring(contentID.length + 1); 
+		  //alert ("chop = " + integrationID );  
+		  integrationID = integrationID.substring(0,integrationID.indexOf("/")); 
+		  alert ("includes a crm subfolder = " + integrationID );  
 		  integrationPath = contentID + "/" + integrationID;	// add the subfolder for the crm-agents content 
 		  contentID = contentID + "/" + integrationID;	// add the subfolder for the crm-agents content 
-
-		  landingPage =  "-guide-ADT";	//https://documentation.five9.com/de/Content/crm-agents/ADT/landing-crm-agents-guide-ADT.htm
+		  //alert ("content ID  = " + contentID );  
+		  //rootURL = url.substring(rootURL.length + 1, url.lastIndexOf("/")  ); 
+		  
+		  //alert ("rootURL = " + rootURL );  
 		}
 
 
 		if(lang == "de") {								// check the content ID against the defined languages for german
-			if(de_langpairs.indexOf(contentID) +1) {	// if the contentID matches a language pair, add the project landing page
+			if(de_langpairs.indexOf(contentID) +1) {
 				alert ("grab content id = " + contentID.slice(0,contentID.search("/") ) );
-				
+				alert ("remove content id = " + contentID.slice(0,integrationID.search("/") ) );
+				//alert ("rootURL = " + rootURL );  
 				url = rootURL + lang + "/" + contentID + "/landing-" + contentID.slice(0,contentID.search("/") ) + ".htm";
-				alert ("root URL = " + rootURL );
-				
 				// url = rootURL + "/" + contentID + "/landing-" + contentID.substring(0,integrationID.length) + ".htm";
-			} else {									// else route to the German landing page 
+			} else {
 				url = rootURL + lang + "/" + "Default.htm";
 			}
 		  alert ("URL = " + url ); 
