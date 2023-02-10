fbfb16f2f5c0d963e117bf0ebb8388b29f2d8612
bobmoonf9 <111763634+bobmoonF9@users.noreply.github.com>
2023-02-10 16:37:03 -0500

    updates

--- Content/js/test.js
+++ Content/js/test.js
@@ -3,7 +3,7 @@
 // supported languages - add the project portion of the URL here 
 var langs = [ 'en-us', 'fr-ca', 'de' ];
 var frca_langpairs = [ 'agent-desktop-plus', 'supervisor-plus', 'technical-reqs' ];
-var de_langpairs = [ 'agent-desktop-plus', 'supervisor-plus', 'crm-agents/ADT', 'crm-agents/NetSuite', 'crm-agents/MSDynamics', 'crm-agents/salesforce' ];
+var de_langpairs = [ 'agent-desktop-plus', 'supervisor-plus', 'crm-agents/ADT', 'crm-agents/NetSuite', 'crm-agents/MSDynamics', 'crm-agents/salesforce', 'crm-agents/ServiceNow' ];
 
 
 
@@ -55,7 +55,10 @@ function use_lang() {					// on change to the language drop down
 		// var url = "https://documentation.five9.com/de/crm-agents/ADT/softphone/preparing-your-station.htm" ;
 		//var url = "https://documentation.five9.com/de/crm-agents/NetSuite/engagement/_ch-processing-digital-engagement-interactions.htm";
 		// var url = "https://documentation.five9.com/de/crm-agents/MSDynamics/engagement/_ch-processing-digital-engagement-interactions.htm"; 
-		 var url = "https://documentation.five9.com/de/crm-agents/salesforce/engagement/_ch-processing-digital-engagement-interactions.htm";
+		// var url = "https://documentation.five9.com/de/crm-agents/salesforce/engagement/_ch-processing-digital-engagement-interactions.htm";
+		// var url = "https://documentation.five9.com/de/crm-agents/salesforce/engagement/_ch-processing-digital-engagement-interactions.htm";
+		// var url = "https://documentation.five9.com/de/cZZrm-agents/salesforce/engagement/_ch-processing-digital-engagement-interactions.htm";
+		var url = "https://documentation.five9.com/fr-ca/agent-desktop-plus/using-adp/customizing-station/sound-alert-settings.htm";
 		//var url = "https://documentation.five9.com/fr-ca/agenzt-desktop-plus/using-adp/customizing-station/sound-alert-settings.htm";
 		// var url = "https://documentation.five9.com/de/crm-agents/salesforce/engagement/_ch-processing-digital-engagement-interactions.htm";
 		
@@ -80,45 +83,44 @@ alert ("rootURL = " + rootURL + "\n url = " + url + "\n curLang = " + curLang + 
 			for (var i = 0; i < de_langpairs.length; i++) {					// cycle through defind pairs
 				if (url.search(de_langpairs[i])  != -1) {					// if the repo id is found, then set the landing page 
 					if (de_langpairs[i]== "agent-desktop-plus") {
-					// if (contentID == "agent-desktop-plus") {
-						landingPage = "-guide-agent-desktop-plus";			
+						landingPage = "agent-desktop-plus/landing-agent-desktop-plus.htm";			
 					} else if (de_langpairs[i] == "supervisor-plus") {
-						landingPage = "-guide-supervisors-plus";
+						landingPage = "supervisor-plus/landing-supervisor-plus.htm";
 					} else if (de_langpairs[i] == "crm-agents/ADT") {
-						landingPage = "-guide-ADT";
+						landingPage = "crm-agents/ADT/landing-crm-agents-guide-ADT.htm";
 					} else if (de_langpairs[i] == "crm-agents/NetSuite") {
-						landingPage = "-guide-NetSuite";
+						landingPage = "crm-agents/NetSuite/landing-crm-agents-guide-NetSuite.htm";
 					} else if (de_langpairs[i] == "crm-agents/MSDynamics") {
-						landingPage = "-guide-MSD";
+						landingPage = "crm-agents/MSDynamics/landing-crm-agents-guide-MSD.htm";
 					} else if (de_langpairs[i] == "crm-agents/salesforce") {
-						landingPage = "-guide-Salesforce";
-					} else if (de_langpairs[i] == "crm-agents/test") {
+						landingPage = "crm-agents/Salesforce/landing-crm-agents-guide-Salesforce.htm";
+					} else if (de_langpairs[i] == "crm-agents/ServiceNow") {
 						landingPage = "hello SUP";
-								
+						//crm-agents/Oracle/landing-crm-agents-guide-Oracle.htm
+						//crm-agents/Zendesk/landing-crm-agents-guide-Zendesk.htm			
 					} else {
 						
 					}
 				}
 			}
-			if (landingPage != -1) {  // if url isn't in the set of language pairs, set landing page to the language home page
-				landingPage = "test";
+			if (landingPage === undefined) {  // if url isn't in the set of language pairs, set landing page to the language home page
+				landingPage = "home.htm";
 			}	
 		} else if (lang == "fr-ca") {
 			for (var i = 0; i < frca_langpairs.length; i++) {
 				if (url.search(frca_langpairs[i])  != -1) {
 					if (frca_langpairs[i]== "agent-desktop-plus") {
-					// if (contentID == "agent-desktop-plus") {
-						landingPage = "-guide-agent-desktop-plus";			// set current language -- IS THIS DUPLICATING LINE 23?
+						landingPage = "agent-desktop-plus/landing-agent-desktop-plus.htm";			// set current language -- IS THIS DUPLICATING LINE 23?
 					} else if (frca_langpairs[i] == "supervisor-plus") {
-						landingPage = "-guide-supervisors-plus";
+						landingPage = "supervisor-plus/landing-supervisor-plus.htm";
 					} else if (frca_langpairs[i] == "crm-agents/ADT") {
-						landingPage = "-guide-ADT";
+						landingPage = "crm-agents/ADT/landing-crm-agents-guide-ADT.htm";
 					} else if (frca_langpairs[i] == "crm-agents/NetSuite") {
-						landingPage = "-guide-NetSuite";
+						landingPage = "crm-agents/NetSuite/landing-crm-agents-guide-NetSuite.htm";
 					} else if (frca_langpairs[i] == "crm-agents/MSDynamics") {
-						landingPage = "-guide-MSD";
+						landingPage = "crm-agents/MSDynamics/landing-crm-agents-guide-MSD.htm";
 					} else if (frca_langpairs[i] == "crm-agents/salesforce") {
-						landingPage = "-guide-Salesforce";
+						landingPage = "crm-agents/Salesforce/landing-crm-agents-guide-Salesforce.htm";
 					} else if (frca_langpairs[i] == "crm-agents/test") {
 						landingPage = "hello SUP";
 								
@@ -127,15 +129,16 @@ alert ("rootURL = " + rootURL + "\n url = " + url + "\n curLang = " + curLang + 
 					}
 				}
 			}
-			if (landingPage != -1) {  // if url isn't in language pair, set to the language home page
-				landingPage = "test";
+			if (landingPage === undefined) {  // if url isn't in language pair, set to the language home page
+				landingPage = "home.htm";
 			}			
 		}
 
 
 
+		url = rootURL + lang + "/Content/" + landingPage  ;   // combine elements to make the landing page URL
 		// url = rootURL + lang + "/" + contentID + "/landing-" + contentID.slice(0,contentID.search("/") ) + ".htm" ;   // combine elements to make the landing page URL
 		// window.location = url;
-		// alert ("URL = " + url ); 
+		 alert ("URL = " + url ); 
 	}
 }
